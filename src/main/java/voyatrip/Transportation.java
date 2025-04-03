469873ec76a671a967163c2ce100870108bd13e7
Jenny Tan <jennytan817@gmail.com>
2025-04-03 17:09:40 +0800

    Add days attribute to Transportation

--- src/main/java/voyatrip/Transportation.java
+++ src/main/java/voyatrip/Transportation.java
@@ -9,17 +9,25 @@ public class Transportation {
     private String name;
     private String mode;
     private Integer budget;
-    private ArrayList<Integer> day;
+    private Integer startDay;
+    private Integer endDay;
+    private ArrayList<Integer> days;
 
 
     private Logger logger = Logger.getLogger(Transportation.class.getName());
 
-    public Transportation(String name, String mode, Integer budget) {
+    public Transportation(String name, String mode, Integer budget, ArrayList<Integer> days) {
         assert budget > 0;
         logger.log(Level.INFO, "Creating Transportation");
         this.name = name;
         this.mode = mode;
         this.budget = budget;
+        this.days = new ArrayList<>();
+
+        //add days to list
+        for (int i = startDay; i <= endDay; i++) {
+            days.add(i);
+        }
 
         logger.log(Level.INFO, "Transportation created");
     }
