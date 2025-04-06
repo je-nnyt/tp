63db88fe55006f3b161d5e79a39e307335bcbee4
choiak <choichaklong@gmail.com>
2025-04-06 15:16:48 +0800

    Adjust testcase

--- src/test/java/voyatrip/DayTest.java
+++ src/test/java/voyatrip/DayTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import voyatrip.command.exceptions.InvalidArgumentValue;
+import voyatrip.command.exceptions.ActivityNotFound;
+import voyatrip.command.exceptions.DuplicatedName;
 import voyatrip.command.exceptions.InvalidIndex;
 
 import java.util.ArrayList;
@@ -27,7 +28,7 @@ public class DayTest {
 
     @Test
     void addActivitySameName_failure() {
-        assertThrowsExactly(InvalidArgumentValue.class, () ->
+        assertThrowsExactly(DuplicatedName.class, () ->
                 day.addActivity(new Activity("Visit the beach", "10:00")));
     }
 
@@ -54,7 +55,7 @@ public class DayTest {
 
     @Test
     void deleteActivityByName_failure() {
-        assertThrowsExactly(InvalidArgumentValue.class, () -> day.deleteActivity("Visit nothing"));
+        assertThrowsExactly(ActivityNotFound.class, () -> day.deleteActivity("Visit nothing"));
     }
 
     @Test
